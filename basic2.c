94e04eeb3700c5b136d7e0b4e2f87e4df9df5072
Diksha1430 <88664752+Diksha1430@users.noreply.github.com>
2021-08-19 20:51:02 +0530

    Add files via upload
    WAP to calculate Simple interest.

---
+++ basic2.c
@@ -0,0 +1,12 @@
+//2. WAP to calculate Simple interest.
+#include<stdio.h>
+int main()
+{
+int p,r,n,a;
+printf("enter value: ");
+scanf("%d%d%d",&p,&r,&n);
+ 
+a=p*r*n;
+printf("\n\na=%d",a);
+return 0;
+}
